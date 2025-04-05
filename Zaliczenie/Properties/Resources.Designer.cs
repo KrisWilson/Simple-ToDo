c0e46a21c441824cf1890be88c6199354d16e4e0
KrisWilson <kris.wilson@op.pl>
2025-04-05 15:53:49 +0200

    Changing some settings

--- Zaliczenie/Properties/Resources.Designer.cs
+++ Zaliczenie/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Zaliczenie.Properties {
+namespace SimpleToDo.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Zaliczenie.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Zaliczenie.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SimpleToDo.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
